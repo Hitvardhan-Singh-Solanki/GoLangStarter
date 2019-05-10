7386b285eed53fd244b02ef17113e0af3f98135f
hitvardhansolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-05-10 16:58:49 +0530

    init and accessing the value of map

---
+++ maps/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
+
+	delete(colors, "red")
+
+	fmt.Println(colors)
+}
