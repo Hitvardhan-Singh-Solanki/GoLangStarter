e1ebbdf2208d93eae76b6b27a33c0e65e4930e71
hitvardhansolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-05-10 13:09:22 +0530

    same package files can call the funtions

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	card := newCardVal()
+	fmt.Println(card)
+	printState()
+}
+
+/**
+Need to tell the return value
+much like solidity
+*/
+func newCardVal() string {
+	return "Five of diamonds"
 }
