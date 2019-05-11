1697a67bbb9ad09e560a1e3e86644abcaede3f28
hitvardhansolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-05-12 04:03:12 +0530

    single threading execution

---
+++ routines/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://golang.org",
+		"https://amazon.com",
+		"https://stackoverflow.com",
+	}
+
+	for _, link := range links {
+		checkStat(link)
+	}
+}
+
+func checkStat(link string) {
+	fmt.Println("Checking stat")
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+		return
+	}
+	fmt.Println(link, "is live")
+}
